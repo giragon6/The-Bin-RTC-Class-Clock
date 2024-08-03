82e1a57a3137d38267de8d125a5b8f197aea44b1
giragon6 <149984595+giragon6@users.noreply.github.com>
2024-08-03 15:20:21 -0400

    Added 3rd party drivers

--- main.py
+++ main.py
@@ -1,50 +1,19 @@
-import time
 from machine import I2C, Pin
-time.sleep(0.1)
+from ds3231 import DS3231 # Driver by cfpwastaken on Github
+from ssd1306 import SSD1306_I2C # Driver forked by stlehmann on Github from Micropython package
+import time
 
-RTC_REG    = const(0x00)
-RTC_ADDR   = const(0x68)
+RTC_I2C = I2C(0, sda=Pin(0), scl=Pin(1), freq=100000)
+rtc = DS3231(RTC_I2C)
 
-SEC_REG    = const(RTC_REG)
-MIN_REG    = const(RTC_REG + 1)
-HOUR_REG   = const(RTC_REG + 2)
-WKDAY_REG  = const(RTC_REG + 3)
-DAY_REG    = const(RTC_REG + 4)
-MONTH_REG  = const(RTC_REG + 5)
-YR_REG     = const(RTC_REG + 6)
+SSD_I2C = I2C(1, sda=Pin(2), scl=Pin(3))
+oled = SSD1306_I2C(128, 64, SSD_I2C)
 
-def bcd_to_decimal(bcd) -> int:
-    return (bcd >> 4) * 10 + (bcd & 0x0F)
+rtc.datetime((24, 8, 3, 2, 42, 50))
+print(rtc.datetime())
 
-def decimal_to_bcd(decimal) -> int:
-    return (decimal // 10) << 4 | (decimal % 10)
-
-#TODO: 4 is being written/read as x01 in the memory -- issue with the writing/reading itself, not conversion/arg
-
-class DS1307:
-    """RTC DS1307 toolbox"""
-    def __init__(self, address: int = 0x68, register: int = 0x00, sda: Pin = Pin(0), scl: Pin = Pin(1), frequency: int = 100000) -> None:
-        self.address = address
-        self.register = register
-        self.i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=frequency)
-
-    def get_datetime(self) -> tuple[int]:
-        print(self.i2c.readfrom_mem(self.address, RTC_REG, 10))
-        return [self.i2c.readfrom_mem(self.address, RTC_REG + i, 1)[0] for i in range(7)] #TODO: Format correctly
-
-    def set_datetime(self, datetime: tuple[int]) -> None:
-        try:
-            if (len(datetime) != 7): raise ValueError 
-            self.i2c.writeto_mem(self.address, RTC_REG, bytes([decimal_to_bcd(v) for v in datetime]))
-        except ValueError:
-            print("Error: Datetime must be in format (seconds, minutes, hours, weekday, day, month, year)")
-
-    def seconds(self, seconds: int = None) -> int:
-        if seconds is not None:
-            self.i2c.writeto_mem(self.address, SEC_REG, bytearray([decimal_to_bcd(seconds)]))
-        return bcd_to_decimal(self.i2c.readfrom_mem(self.address, SEC_REG, 1)[0])
-
-rtc = DS1307()
-
-rtc.set_datetime((1,2,3,4,5,6,7))
-print(rtc.get_datetime())
+while True:
+  oled.fill(0)
+  oled.text(':'.join(str(i) for i in rtc.datetime()[4:7]), 0, 0)
+  oled.show()
+  time.sleep(1)
