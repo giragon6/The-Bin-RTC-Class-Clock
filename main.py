b5cf1e86b543d65f33b00eb897ecf7c2742827c6
giragon6 <149984595+giragon6@users.noreply.github.com>
2024-08-05 14:27:12 -0400

    Began get_time function to fetch time from worldtimeapi.org

--- main.py
+++ main.py
@@ -3,15 +3,28 @@ from ds3231 import DS3231 # Driver by cfpwastaken on Github
 from ssd1306 import SSD1306_I2C # Driver forked by stlehmann on Github from Micropython package
 import time
 import network
+import urequests
+import re
 
 print("Connecting to WiFi", end="")
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('Wokwi-GUEST', '')
-while not sta_if.isconnected() and wlan.status() >= 0:
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect("Wokwi-GUEST", "")
+while not wlan.isconnected():
   print(".", end="")
-  time.sleep(1)
+  time.sleep(0.1)
 print(" Connected!")
+
+def get_time(timezone="America/New_York"):
+  URL = "http://worldtimeapi.org/api/timezone/" + timezone
+  req = urequests.get(URL)
+  datetime_json = req.json()
+  regex = re.compile("/[-:T.]/")
+  datetime = regex.split(datetime_json["datetime"])
+  datetime_tuple = tuple(datetime[0:5])
+  print(datetime_tuple)
+
+get_time()
 
 RTC_I2C = I2C(0, sda=Pin(0), scl=Pin(1), freq=100000)
 rtc = DS3231(RTC_I2C)
@@ -20,7 +33,6 @@ SSD_I2C = I2C(1, sda=Pin(2), scl=Pin(3))
 oled = SSD1306_I2C(128, 64, SSD_I2C)
 
 rtc.datetime((24, 8, 3, 2, 42, 50))
-print(rtc.datetime())
 
 while True:
   oled.fill(0)
