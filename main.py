2774d67ca10406b366abeb64af41a37081042090
giragon6 <149984595+giragon6@users.noreply.github.com>
2024-08-07 22:03:32 -0400

    Finish function to get time until next class, finish time formatting functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from machine import I2C, Pin
+from micropython import const
 from ds3231 import DS3231 # Driver by cfpwastaken on Github
 from ssd1306 import SSD1306_I2C # Driver forked by stlehmann on Github from Micropython package
 import time
@@ -22,34 +23,53 @@ time.sleep(0.1)
 
 time_split_re = re.compile("[-:T.]") # To be used to split time strings as given by worldtimeapi
 
+fetch_json_from_URL: str = lambda URL : urequests.get(URL).json()
+
 def get_time(timezone: str = "America/New_York") -> tuple[int]:
   print("Fetching time...", end="")
-  URL = "http://worldtimeapi.org/api/timezone/" + timezone
-  res = urequests.get(URL)
-  datetime_json = res.json()
+  datetime_json = fetch_json_from_URL("http://worldtimeapi.org/api/timezone/" + timezone)
   split_datetime = time_split_re.split(datetime_json["datetime"]) # (year, month, day, hour, minute, second)
   datetime = tuple([int(i) for i in split_datetime[0:6]]) # Formats for datetime function
   print(" Fetched!")
   return datetime
 
-def to_minutes(time: str | tuple[int]) -> int:
+def get_day_of_week(timezone: str = "America/New_York") -> tuple[int]:
+  print("Fetching day of week...", end="")
+  datetime_json = fetch_json_from_URL("http://worldtimeapi.org/api/timezone/" + timezone)
+  print(" Fetched!")
+  return datetime_json["day_of_week"]
+
+def format_time(hours: int, minutes: int, seconds: int) -> str:
+  return "{h}:{m:02}:{s:02}".format(h=hours, m=minutes, s=seconds)
+
+def to_seconds(time: str | tuple[int]) -> int:
   if type(time) == str:
     split_time: list[str] = time_split_re.split(time)
     hours: int = int(split_time[0])
     minutes: int = int(split_time[1])
+    seconds: int = int(split_time[2])
   elif type(time) == tuple[int]:
     hours: int = time[0]
     minutes: int = time[1]
-  total_minutes: int = hours*60 + minutes
-  return total_minutes
+    seconds: int = time[2]
+  total_seconds: int = hours*3600 + minutes*60 + seconds
+  return total_seconds
 
-def to_time_string(minutes: int) -> str:
-  hours: int = int(minutes / 60)
-  minutes: int =  minutes % 60
-  return (str(hours) + ":" + str(minutes))
+def to_time_string(seconds: int) -> str:
+  hours: int = int(seconds / 3600)
+  minutes: int =  int((seconds - hours * 3600) / 60)
+  seconds: int = seconds % 60
+  return format_time(hours, minutes, seconds)
 
 def get_time_between(now: str, then: str) -> str:
-  return to_time_string(to_minutes(then) - to_minutes(now))
+  now_secs = to_seconds(now)
+  then_secs = to_seconds(then)
+  if then_secs > now_secs: 
+    return to_time_string(then_secs - now_secs)
+  if now_secs > then_secs:
+    _MIDNIGHT_SECS: int = const(86400)
+    return to_time_string(_MIDNIGHT_SECS - (now_secs - then_secs))
+  return to_time_string(0)
 
 # DEBUG FUNCTION -- REMOVE THIS LATER
 # connectivity issues causing fetch from worldtimeapi.org to take unreasonably long
@@ -70,12 +90,13 @@ oled = SSD1306_I2C(128, 64, SSD_I2C)
 
 # rtc.datetime(get_time())
 # rtc.datetime(enter_manual_time())
-rtc.datetime((2024, 8, 7, 13, 32, 40))
+rtc.datetime((2024, 8, 7, 8, 30, 30))
 print("Time synced!")
 
 while True:
   oled.fill(0)
-  oled.text(":".join(str(i) for i in rtc.datetime()[4:7]), 0, 0) # Get hours, minutes, and seconds and format them for screen
-  oled.text("when the", 0, 10)
+  now_time = format_time(*rtc.datetime()[4:7])
+  oled.text(now_time, 0, 0) # Get hours, minutes, and seconds and format them for screen
+  oled.text(get_time_between(now_time, schedule["1"]["blocks"][0]["start_time"]), 0, 10)
   oled.show()
   time.sleep(1)
